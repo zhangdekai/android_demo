f2b630ab725b2d16fee3e2582f069a0dd5bd9673
DK <dekai.zhang@kunlun-inc.com>
2024-03-17 22:49:21 +0800

    add new project for android

--- app/src/main/java/com/example/android_demo/MainActivity2.java
+++ app/src/main/java/com/example/android_demo/MainActivity2.java
@@ -2,11 +2,7 @@ package com.example.android_demo;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
 
